3d7e7d627357b95071315d47f824e6fe11b7e7bc
Nphatic <42347537+Nphatic@users.noreply.github.com>
2018-08-13 14:54:45 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
+    sassLint = require('gulp-sass-lint'),
+    cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/')
+});
+
+gulp.task('compress', function (cb) {
+    pump([
+        gulp.src('./js/**/*.js'),
+        uglify(),
+        gulp.dest('./dist/js')
+    ],
+        cb
+    );
+});
+
+gulp.task('cleancss', function () {
+    return gulp.src('./dist/css/*.css')
+        .pipe(cleanCSS());
+});
+
+gulp.task('default', gulp.series('sass', 'cleancss', 'compress'));
---
+++ js/v1.0/main.js
@@ -0,0 +1,44 @@
+﻿var accordionItems = document.getElementsByClassName('demo-accordion__item');
+var accordionHeadings = document.getElementsByClassName('demo-accordion__item__header')
+
+for (i = 0; i < accordionHeadings.length; i++) {
+    accordionHeadings[i].addEventListener('click', openToggle, false);
+}
+
+function openToggle(e) {
+    var thisItemContentClass = this.parentNode.className;
+    for (i = 0; i < accordionItems.length; i++) {
+        accordionItems[i].className = 'demo-accordion__item closed';
+    }
+    if (thisItemContentClass = 'demo-accordion__item closed') {
+        this.parentNode.className = 'demo-accordion__item open';
+    }
+    e.preventDefault();
+}
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        autoplay: false,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                dots: true,
+                margin: 0
+            },
+            600: {
+                items: 2,
+                nav: false,
+                dots: true,
+                margin: 10
+            },
+            768: {
+                items: 3,
+                nav: true,
+                dots: true,
+                margin: 20
+            }
+        }
+    });
+});
